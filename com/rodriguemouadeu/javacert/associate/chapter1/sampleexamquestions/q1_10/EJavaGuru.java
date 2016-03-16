d80f13b45b1c24e7fc57fdc428c6214caed5d52e
rodrigue <rodrigue.mouadeu@gmail.com>
2016-03-16 11:19:54 -0400

    Sample exam question 1.10

---
+++ com/rodriguemouadeu/javacert/associate/chapter1/sampleexamquestions/q1_10/EJavaGuru.java
@@ -0,0 +1,15 @@
+package com.rodriguemouadeu.javacert.associate.chapter1.sampleexamquestions.q1_10;
+
+import com.rodriguemouadeu.javacert.associate.chapter1.sampleexamquestions.q1_10.courses.Course;
+
+class EJavaGuru {
+
+	public static void main(String args[]){
+
+		Course c = new Course();
+		c.courseName = "Java";
+		System.out.println(c.courseName);
+
+	}
+
+}
---
+++ com/rodriguemouadeu/javacert/associate/chapter1/sampleexamquestions/q1_10/courses/Course.java
@@ -0,0 +1,12 @@
+package com.rodriguemouadeu.javacert.associate.chapter1.sampleexamquestions.q1_10.courses;
+
+class Course{
+	
+	public String courseName;
+
+}
+
+
+
+
+
