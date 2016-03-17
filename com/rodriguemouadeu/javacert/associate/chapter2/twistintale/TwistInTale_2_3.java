8e6cc97d86c1e2a76bd258740d6af367b25828b7
rodrigue <rodrigue.mouadeu@gmail.com>
2016-03-16 21:20:41 -0400

    Twist in the tale 2.3

---
+++ com/rodriguemouadeu/javacert/associate/chapter2/twistintale/TwistInTale_2_3.java
@@ -0,0 +1,15 @@
+package com.rodriguemouadeu.javacert.associate.chapter2.twistintale;
+
+public class TwistInTale_2_3 {
+
+	
+	public static void main(String[] args){
+	
+		int a = 10;
+		a = ++a + a + --a - --a + a++;
+		
+		System.out.println(a);
+		
+	}
+
+}
