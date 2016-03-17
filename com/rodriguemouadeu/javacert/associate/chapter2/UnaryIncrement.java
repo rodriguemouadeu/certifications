f9d67b6ea3e7349c092169bb44a31dac1c3e03d7
rodrigue <rodrigue.mouadeu@gmail.com>
2016-03-16 21:05:50 -0400

    Unary pre and postfix increment

---
+++ com/rodriguemouadeu/javacert/associate/chapter2/UnaryIncrement.java
@@ -0,0 +1,18 @@
+package com.rodriguemouadeu.javacert.associate.chapter2;
+
+
+public class UnaryIncrement {
+
+	public static void main(String[] args) {
+	
+		int a = 20;
+		int b = 10;
+		++a;
+		b++;
+
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
---
+++ com/rodriguemouadeu/javacert/associate/chapter2/UnaryPrefixIncrement.java
@@ -0,0 +1,17 @@
+package com.rodriguemouadeu.javacert.associate.chapter2;
+
+
+public class UnaryPrefixIncrement {
+
+	public static void main(String[] args) {
+	
+		int a = 20;
+		int b = 10;
+		int c = a - ++b;
+
+		System.out.println(c);
+		System.out.println(b);
+
+	}
+
+}
