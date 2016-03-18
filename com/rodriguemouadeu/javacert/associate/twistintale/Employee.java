40da57752aeba8263b83fb205d5177d008494ada
rodrigue <rodrigue.mouadeu@gmail.com>
2016-03-18 10:45:54 -0400

    Recursive constructor call

---
+++ com/rodriguemouadeu/javacert/associate/twistintale/Employee.java
@@ -0,0 +1,22 @@
+package com.rodriguemouadeu.javacert.associate.twistintale;
+
+
+class Employee  {
+
+	String name;
+	int age;
+
+	Employee(){
+	
+		this();
+
+	}
+
+	Employee(String newName, int newAge) {
+		name = newName;
+		age = newAge;
+	}
+
+
+}
+
---
+++ com/rodriguemouadeu/javacert/associate/twistintale/TwistInTale_3_2.java
@@ -0,0 +1,14 @@
+package com.rodriguemouadeu.javacert.associate.twistintale;
+
+
+class TwistInTale_3_2  {
+
+
+	public static void main(String[] args ){
+
+		Employee emp = new Employee();
+
+	}
+
+}
+
