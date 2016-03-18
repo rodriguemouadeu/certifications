f87deae69c289242df206553f1c9d8790e06d240
rodrigue <rodrigue.mouadeu@gmail.com>
2016-03-18 11:17:50 -0400

    Twist in the tale 3.3

---
+++ com/rodriguemouadeu/javacert/associate/twistintale/Phone.java
@@ -0,0 +1,15 @@
+package com.rodriguemouadeu.javacert.associate.twistintale;
+
+class Phone {
+
+	public String model;
+	double weight;
+	public void setWeight(double w){ 
+		weight = w;
+	}
+
+	public double getWeight(){
+		return weight;
+	}	
+
+}
---
+++ com/rodriguemouadeu/javacert/associate/twistintale/TwistInTale_3_3.java
@@ -0,0 +1,20 @@
+package com.rodriguemouadeu.javacert.associate.twistintale;
+
+class TwistInTale_3_3 {
+
+	public static void main(String[] args){
+		Phone phone = new Phone();
+
+		phone.setWeight(-12.23);
+		System.out.println(phone.getWeight());		
+
+		phone.setWeight(77712.23);
+		System.out.println(phone.getWeight());		
+		
+		phone.setWeight(12.23);
+		System.out.println(phone.getWeight());		
+
+
+	} 
+
+}
