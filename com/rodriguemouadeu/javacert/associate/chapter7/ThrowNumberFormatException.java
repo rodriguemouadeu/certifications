3d3b5a410c3187741998644f291683bf5fb0c5f9
rodrigue <rodrigue.mouadeu@gmail.com>
2016-04-06 05:32:53 -0400

    Throwing NumberFormatException

---
+++ com/rodriguemouadeu/javacert/associate/chapter7/ThrowNumberFormatException.java
@@ -0,0 +1,24 @@
+package com.rodriguemouadeu.javacert.associate.chapter7;
+
+import java.io.*;
+
+class ThrowNumberFormatException {
+
+
+	public static void main(String[] args){
+		System.out.println(convertToNum("16b"));
+		System.out.println(convertToNum("65v"));
+	}
+
+	static int convertToNum(String val){
+		int num = 0;	
+		try{
+			num = Integer.parseInt(val, 16);
+		} catch (NumberFormatException e){
+			throw new NumberFormatException(val+" cannot be converted to hexadecimal number !!!");
+		} finally {
+ 		}
+		
+		return num;
+	}
+}
