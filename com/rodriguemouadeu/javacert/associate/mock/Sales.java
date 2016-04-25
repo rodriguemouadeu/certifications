700dd30f58e5de390aef86b05830deffe8e2eaaa
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2016-04-25 14:53:27 -0400

    ME-Q13

---
+++ com/rodriguemouadeu/javacert/associate/mock/Sales.java
@@ -0,0 +1,13 @@
+package com.rodriguemouadeu.javacert.associate.mock;
+
+class Sales {
+
+	public static void main(String... args){
+
+		int a = 1;
+
+		System.out.println(a++ + ++a + ++a);
+	
+	}
+
+}
