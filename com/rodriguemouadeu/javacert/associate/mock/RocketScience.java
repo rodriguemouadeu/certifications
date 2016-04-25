2461769dc83a0cb83ee0eb974aacc68746d766bc
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2016-04-25 14:52:21 -0400

    ME-Q8

---
+++ com/rodriguemouadeu/javacert/associate/mock/RocketScience.java
@@ -0,0 +1,15 @@
+package com.rodriguemouadeu.javacert.associate.mock;
+
+class RocketScience {
+
+	public static void main(String... args){
+
+		int a = 0;
+		while(a == a++){
+			a++;
+			System.out.println(a);
+		}
+	
+	}
+
+}
