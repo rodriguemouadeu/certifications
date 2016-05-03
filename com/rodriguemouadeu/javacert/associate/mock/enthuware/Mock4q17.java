4b4fd144d9fa5a708a5cb100275933e06bba8750
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2016-05-03 19:58:04 -0400

    Mock 4 question 17

---
+++ com/rodriguemouadeu/javacert/associate/mock/enthuware/Mock4q17.java
@@ -0,0 +1,11 @@
+package com.rodriguemouadeu.javacert.associate.mock.enthuware;
+
+class Mock4q17 {
+
+   public static void main(String args[ ] ){
+      byte b = -128 ;
+      int i = b ;
+      b = (byte) i;
+      System.out.println(i+" "+b);
+   }
+}
