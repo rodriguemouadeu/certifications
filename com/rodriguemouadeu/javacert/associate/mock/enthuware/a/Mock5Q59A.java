f1a95d26fc027170a85a05d801c80a55686a9885
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2016-05-07 23:09:47 -0400

    Default access modifier test

---
+++ com/rodriguemouadeu/javacert/associate/mock/enthuware/a/Mock5Q59A.java
@@ -0,0 +1,8 @@
+package com.rodriguemouadeu.javacert.associate.mock.enthuware.a;
+
+public class Mock5Q59A {
+    
+	Mock5Q59A(){}
+
+	public void print(){ System.out.println("A"); }
+}
---
+++ com/rodriguemouadeu/javacert/associate/mock/enthuware/b/Mock5Q59B.java
@@ -0,0 +1,14 @@
+package com.rodriguemouadeu.javacert.associate.mock.enthuware.b;
+
+import com.rodriguemouadeu.javacert.associate.mock.enthuware.a.*;
+
+public class Mock5Q59B extends Mock5Q59A {
+    
+	Mock5Q59B(){}
+
+	public void print(){ System.out.println("B"); }
+
+    public static void main(String[] args) {
+    	new Mock5Q59B().print();
+    }
+}
