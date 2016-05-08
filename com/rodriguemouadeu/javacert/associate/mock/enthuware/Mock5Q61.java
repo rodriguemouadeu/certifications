534cb17de10f05d082a0908e3bd67afc0a43afb2
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2016-05-07 23:26:36 -0400

    switch statement's break condition

---
+++ com/rodriguemouadeu/javacert/associate/mock/enthuware/Mock5Q61.java
@@ -0,0 +1,18 @@
+package com.rodriguemouadeu.javacert.associate.mock.enthuware;
+
+public class Mock5Q61 {
+
+	public static void main(String args[]) {
+		int k = 9, s = 5;	
+		switch(k){
+			default :
+				if( k == 10) { s = s*2; }
+				else{
+					s = s+4;
+					break;
+				}
+			case 7 : s = s+3;
+		}
+		System.out.println(s);
+	}
+}
