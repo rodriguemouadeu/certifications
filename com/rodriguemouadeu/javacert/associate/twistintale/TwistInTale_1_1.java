4c08670d18ebe98f9817e27975a16ca2c0da873f
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2016-05-16 11:55:18 -0400

    Multiple classes in same file

---
+++ com/rodriguemouadeu/javacert/associate/twistintale/TwistInTale_1_1.java
@@ -0,0 +1,13 @@
+package com.rodriguemouadeu.javacert.associate.twistintale;
+
+class TwistInTale_1_1 {
+
+}
+
+public interface Printable{
+
+}
+
+interface Movable{
+
+}
---
+++ com/rodriguemouadeu/javacert/associate/twistintale/TwistInTale_1_2.java
@@ -0,0 +1,14 @@
+package com.rodriguemouadeu.javacert.associate.twistintale;
+
+public interface TwistInTale_1_2 {
+}
+
+interface Printable{
+}
+
+interface Movable{
+}
+
+class MyClass{}
+
+class Car{}
